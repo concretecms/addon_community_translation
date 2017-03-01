9182204894bf5541db6a82db7e5b1f27daa086ac
Michele Locati <michele@locati.it>
2017-03-01 14:02:32 +0100

    Done with packages search

--- blocks/search_packages/controller.php
+++ blocks/search_packages/controller.php
@@ -3,10 +3,12 @@ namespace Concrete\Package\CommunityTranslation\Block\SearchPackages;
 
 use CommunityTranslation\Controller\BlockController;
 use CommunityTranslation\Entity\Package as PackageEntity;
+use CommunityTranslation\Entity\Package\Version as PackageVersionEntity;
+use CommunityTranslation\Repository\Locale as LocaleRepository;
 use CommunityTranslation\Repository\Package as PackageRepository;
+use CommunityTranslation\Repository\Stats as StatsRepository;
 use CommunityTranslation\Service\Access;
 use CommunityTranslation\UserException;
-use Concrete\Core\Http\ResponseFactoryInterface;
 
 class Controller extends BlockController
 {
@@ -81,59 +83,171 @@ class Controller extends BlockController
      */
     protected function getInstanceSpecificTasks()
     {
-        return '*';
+        return [
+            '!action_package',
+        ];
+    }
+
+    private function setCommonSets()
+    {
+        $this->set('token', $this->app->make('token'));
+    }
+
+    /**
+     * @param int $perc
+     *
+     * @return string
+     */
+    private function percToColor($perc)
+    {
+        if ($perc < 50) {
+            $r = 255;
+            $g = (int) round(5.1 * $perc);
+            $b = 0;
+        } else {
+            $g = 255;
+            $r = (int) round(510 - 5.10 * $perc);
+            $b = 0;
+        }
+        $h = ($r << 16) + ($g << 8) + ($b << 0);
+
+        return '#' . str_pad(dechex($h), 6, '0', STR_PAD_LEFT);
+    }
+
+    private function setPackageVersionSets(PackageVersionEntity $packageVersion)
+    {
+        $allLocales = $this->app->make(LocaleRepository::class)->getApprovedLocales();
+        $myLocales = [];
+        $accessHelper = $this->app->make(Access::class);
+        /* @var Access $accessHelper */
+        if ($accessHelper->isLoggedIn()) {
+            foreach ($allLocales as $locale) {
+                if ($accessHelper->getLocaleAccess($locale, 'current') >= Access::TRANSLATE) {
+                    $myLocales[] = $locale;
+                }
+            }
+        }
+        $stats = $this->app->make(StatsRepository::class)->get($packageVersion, $allLocales);
+        $localeInfos = [];
+        foreach ($allLocales as $locale) {
+            $localeStats = null;
+            foreach ($stats as $s) {
+                if ($s->getLocale() === $locale) {
+                    $localeStats = $s;
+                    break;
+                }
+            }
+            $localeInfos[$locale->getID()] = [
+                'perc' => ($localeStats === null) ? 0 : $localeStats->getPercentage(true),
+                'color' => $this->percToColor(($localeStats === null) ? 0 : $localeStats->getPercentage(true)),
+                'untranslated' => ($localeStats === null) ? null : $localeStats->getUntranslated(),
+            ];
+        }
+        $this->set('allLocales', $allLocales);
+        $this->set('myLocales', $myLocales);
+        $this->set('localeInfos', $localeInfos);
+        $this->set('showLoginMessage', !$accessHelper->isLoggedIn());
+        $config = $this->app->make('community_translation/config');
+        $this->set('onlineTranslationPath', $config->get('options.onlineTranslationPath'));
     }
 
     public function view()
     {
-        $this->requireAsset('selectize');
-        $this->set('token', $this->app->make('token'));
-        $this->set('initialData', null);
-        if ($this->preloadPackageHandle !== '') {
+        $package = null;
+        if ($this->preloadPackageHandle === '') {
+            $package = null;
+        } else {
             $package = $this->app->make(PackageRepository::class)->findOneBy(['handle' => $this->preloadPackageHandle]);
-            if ($package !== null) {
-                $this->set('initialData', $this->resultToJSON($package));
-            }
+        }
+        if ($package === null) {
+            $packageVersions = null;
+        } else {
+            $packageVersions = $package->getSortedVersions(true);
+        }
+        if (empty($packageVersions)) {
+            $packageVersion = null;
+        } else {
+            $packageVersion = current($packageVersions);
+        }
+        $this->setCommonSets();
+        $this->set('package', $package);
+        $this->set('packageVersions', $packageVersions);
+        $this->set('packageVersion', $packageVersion);
+        if ($packageVersion !== null) {
+            $this->setPackageVersionSets($packageVersion);
         }
     }
 
     public function action_search()
     {
-        $rf = $this->app->make(ResponseFactoryInterface::class);
         try {
             $token = $this->app->make('token');
-            if (!$token->validate('comtra_search-package')) {
+            if (!$token->validate('comtra_search_packages-search')) {
                 new UserException($token->getErrorMessage());
             }
-            $search = $this->post('search');
-            $search = is_string($search) ? trim($search) : '';
-            if ($search === '') {
-                new UserException(t('Please specify what you would like to search'));
-            }
-            if (strlen($search) < self::MIN_SEARCH_LENGTH) {
-                new UserException(t('Please be more specific with your search'));
-            }
-            $repo = $this->app->make(PackageRepository::class);
-            $packages = $repo->createQueryBuilder('p')
-                ->where('p.name like :search')
-                ->orWhere('p.handle like :search')
-                ->setParameter('search', '%' . $search . '%')
-                ->setMaxResults(self::MAX_SEARCH_RESULTS)
-                ->getQuery()->execute();
-            $result = [];
-            foreach ($packages as $package) {
-                $r = $this->resultToJSON($package);
-                if ($r !== null) {
-                    $result[] = $r;
+            $text = $this->request->query->get('text');
+            $words = is_string($text) ? $this->getSearchWords($text) : [];
+            if (count($words) > 0) {
+                $this->set('searchText', $text);
+                if (mb_strlen(implode('', $words)) < static::MIN_SEARCH_LENGTH) {
+                    throw new UserException(t('Please be more specific with your search'));
+                }
+                $qb = $this->buildSearchQuery($words);
+                $packages = $qb->getQuery()->execute();
+                switch (count($packages)) {
+                    case 0:
+                        throw new UserException(t('No results found'));
+                    case 1:
+                        $this->action_package($packages[0]->getHandle());
+                        break;
+                    default:
+                        $this->set('foundPackages', $packages);
+                        break;
                 }
             }
-
-            return $rf->json($result);
         } catch (UserException $x) {
-            return $rf->json([
-                'error' => true,
-                'errors' => [$x->getMessage()],
-            ]);
+            $this->set('searchError', h($x->getMessage()));
+        }
+
+        $this->setCommonSets();
+    }
+
+    public function action_package($handle = '', $version = '')
+    {
+        $package = null;
+        $packageVersions = null;
+        $packageVersion = null;
+        if (is_string($handle) && $handle !== '') {
+            $package = $this->app->make(PackageRepository::class)->findOneBy(['handle' => $handle]);
+            if ($package === null) {
+                $this->set('showWarning', h(t('Unable to find a package with handle "%s"', $handle)));
+            } else {
+                $packageVersions = $package->getSortedVersions(true);
+                if (!empty($packageVersions)) {
+                    if (is_string($version) && $version !== '') {
+                        foreach ($packageVersions as $pv) {
+                            if ($pv->getVersion() === $version) {
+                                $packageVersion = $pv;
+                                break;
+                            }
+                        }
+                        if ($packageVersion === null) {
+                            $this->set('showWarning', h(t('The package "%1$s" does not have a version "%2$s"', $package->getDisplayName(), $version)));
+                        }
+                    } else {
+                        $packageVersion = current($packageVersions);
+                    }
+                }
+            }
+        } else {
+            $package = null;
+        }
+        $this->setCommonSets();
+        $this->set('package', $package);
+        $this->set('packageVersions', $packageVersions);
+        $this->set('packageVersion', $packageVersion);
+        if ($packageVersion !== null) {
+            $this->setPackageVersionSets($packageVersion);
         }
     }
 
@@ -156,5 +270,59 @@ class Controller extends BlockController
         }
 
         return $result;
+    }
+
+    /**
+     * @param string $text
+     *
+     * @return string[]
+     */
+    private function getSearchWords($text)
+    {
+        $result = [];
+        if (is_string($text)) {
+            $text = trim(preg_replace('/[\W_]+/u', ' ', $text));
+            if ($text !== '') {
+                $words = explode(' ', mb_strtolower($text));
+                $words = array_values(array_unique($words));
+                $duplicates = [];
+                for ($i = 0; $i < count($words); ++$i) {
+                    for ($j = 0; $j < count($words); ++$j) {
+                        if ($i !== $j && @mb_stripos($words[$i], $words[$j]) === 0) {
+                            $duplicates[] = $words[$j];
+                        }
+                    }
+                }
+                if (!empty($duplicates)) {
+                    $words = array_values(array_diff($words, $duplicates));
+                }
+                $result = $words;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string[] $words
+     *
+     * @return \Doctrine\ORM\QueryBuilder
+     */
+    private function buildSearchQuery(array $words)
+    {
+        $qb = $this->app->make(PackageRepository::class)->createQueryBuilder('p');
+        $expr = $qb->expr();
+        $orFields = $expr->orX();
+        $counter = 0;
+        foreach (['p.handle', 'p.name'] as $fieldIndex => $fieldName) {
+            $and = $expr->andX();
+            foreach ($words as $word) {
+                $and->add($expr->like($fieldName, $expr->literal('%' . $word . '%')));
+            }
+            $orFields->add($and);
+        }
+        $qb->where($orFields)->setMaxResults(static::MAX_SEARCH_RESULTS);
+
+        return $qb;
     }
 }
--- blocks/search_packages/view.php
+++ blocks/search_packages/view.php
@@ -1,42 +1,203 @@
 <?php
 defined('C5_EXECUTE') or die('Access Denied.');
 
+/* @var int $bID */
 /* @var Concrete\Core\Validation\CSRF\Token $token */
 /* @var Concrete\Core\Block\View\BlockView $view */
 
-/* @var array|null $initialData */
-?>
-<select id="comtra-search_package" placeholder="<?= t('Search for packages') ?>">
-    <?php
-    if ($initialData !== null) {
-        ?><option value="<?= h($initialData['handle']) ?>" selected="selected"><?= h($initialData['name']) ?></option><?php
-    }
+/* @var CommunityTranslation\Entity\Package|null $package */
+/* @var CommunityTranslation\Entity\Package\Version[]|null $packageVersions */
+/* @var CommunityTranslation\Entity\Package\Version|null $packageVersion */
+
+if (isset($showWarning) && $showWarning !== '') {
     ?>
-</select>
-<script>
-$(document).ready(function() {
+    <div class="alert alert-danger" role="alert">
+        <p><?= $showWarning ?></p>
+    </div>
+    <?php
+}
 
-$('#comtra-search_package').selectize({
-    valueField: 'handle',
-    labelField: 'name',
-    create: false,
-    load: function(query, callback) {
-        if (!query.length) return callback();
-        $.ajax({
-            cache: true,
-            data: {ccm_token: <?= json_encode($token->generate('comtra_search-package'))?>, search: query},
-            dataType: 'json',
-            method: 'POST',
-            url: <?= json_encode((string) $view->action('search')) ?>,
-            error: function() {
-                callback();
-            },
-            success: function(res) {
-                callback(res);
-            }
-        });
-    }
-});
+if ($package !== null) {
+    ?>
+    <ul class="nav-tabs nav" id="comtra_search_packages-tab-headers">
+        <li class="active"><a href="#" data-tab="comtra_search_packages-tab-package"><?= h($package->getDisplayName()) ?></a></li>
+        <li><a href="#" data-tab="comtra_search_packages-tab-search"><?= t('Search other packages') ?></a></li>
+    </ul>
+    <div class="tab-content" id="comtra_search_packages-tab-panes" style="padding-top: 20px">
+        <div class="tab-pane active" id="comtra_search_packages-tab-package">
+            <div class="container">
+                <div class="col-sm-12">
+                    <?php
+                    if (empty($packageVersions)) {
+                        ?>
+                        <div class="alert alert-danger" role="alert">
+                            <p><?= h(t('The package "%s" doesn\'t have any version', $package->getDisplayName())) ?></p>
+                        </div>
+                        <?php
+                    } else {
+                        ?>
+                        <form class="form-inline" onsubmit="return false">
+                            <div class="form-group">
+                                <label for="comtra_search_packages-version"><?= t('Package version') ?></label>
+                                <select class="form-control" id="comtra_search_packages-version" onchange="if (this.value) { window.location.href = this.value; this.disabled = true }">
+                                    <?php
+                                    if ($packageVersion === null) {
+                                        ?><option value="" selected="selected"><?= t('Please Select') ?></option><?php
+                                    }
+                                    foreach ($packageVersions as $pv) {
+                                        if ($pv === $packageVersion) {
+                                            ?><option value="" selected="selected"><?= h($pv->getDisplayVersion()) ?></option><?php
+                                        } else {
+                                            ?><option value="<?= h(preg_replace('/\/' . $bID . '(\/?)$/', '$1', $view->action('package', $package->getHandle(), $pv->getVersion()))) ?>"><?= h($pv->getDisplayVersion()) ?></option><?php
+                                        }
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </form>
+                        <?php
+                        if ($packageVersion !== null) {
+                            /* @var CommunityTranslation\Entity\Locale[] $allLocales */
+                            /* @var CommunityTranslation\Entity\Locale[] $myLocales */
+                            /* @var bool $showLoginMessage */
+                            /* @var array $localeInfos */
+                            /* @var string $onlineTranslationPath */
+                            ?>
+                            <div class="panel panel-primary" style="margin-top: 20px">
+                                <div class="panel-heading">
+                                    <?= t('Translations for %s', h($packageVersion->getDisplayName())) ?>
+                                </div>
+                                <table class="table table-striped">
+                                    <col />
+                                    <col width="1" />
+                                    <thead>
+                                        <th><?= t('Language') ?></th>
+                                        <th></th>
+                                        <th><?= t('Progress') ?></th>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $sections = [];
+                                        if (empty($myLocales) || count($myLocales) === $allLocales) {
+                                            $sections[''] = $allLocales;
+                                        } else {
+                                            $sections[t('My languages')] = $myLocales;
+                                            $sections[t('Other languages')] = array_diff($allLocales, $myLocales);
+                                        }
+                                        $sections = array_filter($sections);
+                                        foreach ($sections as $sectionName => $locales) {
+                                            if (count($sections) > 1) {
+                                                ?><tr><th colspan="3" class="text-center"><?= $sectionName ?></th></tr><?php
+                                            }
+                                            foreach ($locales as $locale) {
+                                                $percClass = 'progress-bar-info';
+                                                $localeInfo = $localeInfos[$locale->getID()];
+                                                $whyNotTranslatable = null;
+                                                if (!in_array($locale, $myLocales, true)) {
+                                                    if ($showLoginMessage) {
+                                                        $whyNotTranslatable = t('You need to log-in in order to translate');
+                                                    } else {
+                                                        $whyNotTranslatable = t('You don\'t belong to this translation group');
+                                                    }
+                                                }
+                                                if ($whyNotTranslatable === null) {
+                                                    $translateLink = URL::to($onlineTranslationPath, $packageVersion->getID(), $locale->getID());
+                                                    $translateOnclick = '';
+                                                    $translateClass = 'btn-primary';
+                                                } else {
+                                                    $translateLink = '#';
+                                                    $translateOnclick = ' onclick="' . h('window.alert(' . json_encode($whyNotTranslatable) . '); return false') . '"';
+                                                    $translateClass = 'btn-default';
+                                                }
+                                                if ($localeInfo['perc'] > 0 && $localeInfo['perc'] < 10) {
+                                                    $percClass .= ' progress-bar-minwidth1';
+                                                } elseif ($perc >= 10 && $perc < 100) {
+                                                    $percClass .= ' progress-bar-minwidth2';
+                                                }
+                                                ?>
+                                                <tr>
+                                                    <td><?= $locale->getDisplayName() ?></td>
+                                                    <td>
+                                                        <a href="<?= h($translateLink) ?>"<?= $translateOnclick ?> class="btn btn-sm <?= $translateClass ?>" style="padding: 5px 10px"><?= t('Translate') ?></a>
+                                                    </td>
+                                                    <td><div class="progress" style="margin: 0" title="<?= t2('%s untranslated string', '%s untranslated strings', $localeInfo['untranslated']) ?>">
+                                                        <div class="progress-bar <?= $percClass ?>" role="progressbar" style="width: <?= $localeInfo['perc'] ?>%; background-color: <?= $localeInfo['color'] ?>">
+                                                            <span><?= $localeInfo['perc'] ?></span>
+                                                        </div>
+                                                    </div></td>
+                                                </tr>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                    </tbody>
+                                </table>
 
-});
-</script>
+                            </div>
+                            <?php
+                        }
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane" id="comtra_search_packages-tab-search" style="display: none"><?php
+}
+
+?>
+<form class="form-inline" action="<?= $view->action('search') ?>" method="GET">
+    <?php $token->output('comtra_search_packages-search') ?>
+    <div class="form-group">
+        <label for="comtra_search_packages-search-text"><?= t('Search package') ?></label>
+        <input type="search" name="text" class="form-control" id="comtra_search_packages-search-text" value="<?= isset($searchText) ? h($searchText) : ''?>" placeholder="<?= t('Search by handle or name') ?>" required="required" pattern="\s*\w+.*">
+        <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i></button>
+    </div>
+</form>
+<?php
+if (isset($searchError) && $searchError !== '') {
+    ?>
+    <div class="alert alert-warning" role="alert">
+        <p><?= $searchError ?></p>
+    </div>
+    <?php
+}
+if (!empty($foundPackages)) {
+    /* @var CommunityTranslation\Entity\Package $foundPackages */
+    ?>
+    <div id="searchResults">
+        <?php
+        foreach ($foundPackages as $foundPackage) {
+            $url = preg_replace('/\/' . $bID . '(\/?)$/', '$1', $view->action('package', $foundPackage->getHandle()));
+            ?>
+            <div class="searchResult">
+                <h3><a href="<?= h($url) ?>"><?= h($foundPackage->getDisplayName()) ?></a></h3>
+                <p class="text-muted">
+                    <?= h(t('Package handle: %s', $foundPackage->getHandle())) ?><br />
+                    <?= h(t('Number of available versions: %d', count($foundPackage->getVersions()))) ?>
+                </p>
+            </div>
+            <?php
+        }
+        ?>
+    </div>
+    <?php
+}
+
+if ($package !== null) {
+    ?>
+        </div>
+    </div>
+    <script>
+    $(document).ready(function() {
+        $('#comtra_search_packages-tab-headers a').on('click', function(e) {
+            e.preventDefault();
+            $('#comtra_search_packages-tab-headers>li').removeClass('active');
+            $('#comtra_search_packages-tab-panes>.tab-pane').hide().removeClass('active');
+            var $a = $(this);
+            $a.closest('li').addClass('active');
+            $('#comtra_search_packages-tab-panes>#' + $a.data('tab')).show().addClass('active');
+        })
+    });
+    </script>
+    <?php
+}
