8aec243e89b2f1e1e96387c0c59f810c18d0634c
Michele Locati <michele@locati.it>
2017-03-01 21:12:36 +0100

    Show link to profile page

--- src/Service/User.php
+++ src/Service/User.php
@@ -1,12 +1,27 @@
 <?php
 namespace CommunityTranslation\Service;
 
+use Concrete\Core\Application\Application;
 use Concrete\Core\Entity\User\User as ConcreteUserEntity;
 use Concrete\Core\User\User as ConcreteUser;
 use Concrete\Core\User\UserInfo;
+use Concrete\Core\User\UserInfoRepository;
 
 class User
 {
+    /**
+     * @var Application
+     */
+    protected $app;
+
+    /**
+     * @param Application $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
     /**
      * Format a username.
      *
@@ -18,6 +33,7 @@ class User
     {
         $id = null;
         $name = '';
+        $userInfo = null;
         if (isset($user) && $user) {
             if (is_int($user) || (is_string($user) && is_numeric($user))) {
                 $user = \User::getByUserID($user);
@@ -28,17 +44,37 @@ class User
             } elseif ($user instanceof UserInfo && $user->getUserID()) {
                 $id = (int) $user->getUserID();
                 $name = $user->getUserName();
+                $userInfo = $user;
             } elseif ($user instanceof ConcreteUserEntity && $user->getUserID()) {
                 $id = (int) $user->getUserID();
                 $name = $user->getUserName();
             }
         }
         if ($id === null) {
-            return '<i class="comtra-user comtra-user-removed">' . t('removed user') . '</i>';
+            $result = '<i class="comtra-user comtra-user-removed">' . t('removed user') . '</i>';
         } elseif ($id == USER_SUPER_ID) {
-            return '<i class="comtra-user comtra-user-system">' . t('system') . '</i>';
+            $result = '<i class="comtra-user comtra-user-system">' . t('system') . '</i>';
         } else {
-            return '<span class="comtra-user comtra-user-found">' . h($name) . '</span>';
+            $profileURL = null;
+            if ($userInfo === null) {
+                $userInfo = $this->app->make(UserInfoRepository::class)->getByID($id);
+            }
+            if ($userInfo !== null) {
+                $profileURL = $userInfo->getUserPublicProfileUrl();
+            }
+            if ($profileURL === null) {
+                $result = '<span';
+            } else {
+                $result = '<a href="' . h((string) $profileURL) . '"';
+            }
+            $result .= ' class="comtra-user comtra-user-found">' . h($name);
+            if ($profileURL === null) {
+                $result .= '</span>';
+            } else {
+                $result .= '</a>';
+            }
         }
+
+        return $result;
     }
 }
