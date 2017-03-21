683b0f6ba784a24fc109d24e49f275e4726530b6
Michele Locati <michele@locati.it>
2017-03-21 21:21:05 +0100

    Continue notify-packages CLI command

--- src/Console/Command/NotifyPackageVersionsCommand.php
+++ src/Console/Command/NotifyPackageVersionsCommand.php
@@ -9,13 +9,19 @@ use CommunityTranslation\Repository\Package as PackageRepository;
 use Concrete\Core\User\UserInfo;
 use Concrete\Core\User\UserInfoRepository;
 use Concrete\Core\User\UserList;
+use DateTime;
 use Doctrine\ORM\EntityManager;
+use Exception;
 
 class NotifyPackageVersionsCommand extends Command
 {
     const RETURN_CODE_ON_FAILURE = 3;
 
-    
+    /**
+     * @var DateTime|null
+     */
+    private $timeLimit;
+
     /**
      * @var EntityManager|null
      */
@@ -32,33 +38,61 @@ class NotifyPackageVersionsCommand extends Command
     private $userInfoRepository;
 
     /**
-     * PackageRepository
+     * PackageRepository.
      */
     private $packageRepository;
-    
+
     /**
-     * NotificationRepository
+     * NotificationRepository.
      */
     private $notificationRepository;
-    
+
+    /**
+     * The default maximum age (in days) of the new packages/package versions that may raise notifications.
+     *
+     * @var int
+     */
+    const DEFAULT_MAX_AGE = 3;
 
     protected function configure()
     {
+        $errExitCode = static::RETURN_CODE_ON_FAILURE;
         $this
             ->setName('ct:notify-packages')
             ->setDescription('Prepare notifications about new packages, new package versions and updated packages')
+            ->addOption('max-age', 'a', InputOption::VALUE_REQUIRED, 'The maximum age (in days) of the packages and package versions', static::DEFAULT_MAX_AGE)
+            ->setHelp(<<<EOT
+This command prepare notifications about new packages, new package versions and package versions with updated strings.
+
+Returns codes:
+  0 operation completed successfully
+  $errExitCode errors occurred
+EOT
+            )
         ;
     }
 
     protected function executeWithLogger()
     {
+        $this->readParameters();
         if ($this->acquireLock(0) === false) {
             throw new Exception('Failed to acquire lock');
         }
         $this->initializeProcessing();
         $this->notifyNewPackages();
+        $this->notifyNewPackageVersions();
+        $this->notifyUpdatedPackageVersions();
         $this->finishProcessing();
         $this->releaseLock();
+    }
+
+    private function readParameters()
+    {
+        $maxAge = (int) $this->input->getOption('max-age');
+        if ($maxAge <= 0) {
+            throw new Exception('Invalid value of the max-age parameter (it must be an integer greater than 0)');
+        }
+        $this->timeLimit = new DateTime("-$maxAge days");
     }
 
     private function initializeProcessing()
@@ -110,7 +144,8 @@ class NotifyPackageVersionsCommand extends Command
         $qb = $this->packageRepository->createQueryBuilder('p');
         $qb
             ->leftJoin(PackageSubscriptionEntity::class, 's', 'WITH', 'p.id = s.package AND :user = s.user')->setParameter('user', $userInfo->getEntityObject())
-            ->where($qb->expr()->isNull('s.user'));
+            ->where($qb->expr()->isNull('s.user'))
+            ->andWhere('p.createdOn >= :timeLimit')->setParameter('timeLimit', $this->timeLimit);
         $q = $qb->getQuery();
         foreach ($q->iterate() as $packageRows) {
             yield $packageRows[0];
@@ -125,5 +160,15 @@ class NotifyPackageVersionsCommand extends Command
         $this->em->flush($pse);
         $this->em->detach($pse);
         $this->notificationRepository->newTranslatablePackage($package->getID(), $userInfo->getUserID());
+    }
+
+    private function notifyNewPackageVersions()
+    {
+        throw new Exception('@todo');
+    }
+
+    private function notifyUpdatedPackageVersions()
+    {
+        throw new Exception('@todo');
     }
 }
