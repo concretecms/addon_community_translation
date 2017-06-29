f36230c7799dd82fbbadb466eb98697f7e9780f0
Michele Locati <michele@locati.it>
2017-06-29 11:48:16 +0200

    Version 0.4.3

--- controller.php
+++ controller.php
@@ -50,7 +50,7 @@ class Controller extends Package
      *
      * @var string
      */
-    protected $pkgVersion = '0.4.2';
+    protected $pkgVersion = '0.4.3';
 
     /**
      * The mapping between RelativeDirectory <-> Namespace to autoload package classes.
