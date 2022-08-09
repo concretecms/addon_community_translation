ed978ab733a4788dc6cb079ad513de4c503082e9
Michele Locati <michele@locati.it>
2022-08-09 11:07:35 +0200

    Let remote packages log be less verbose

--- src/RemotePackage/DownloadException.php
+++ src/RemotePackage/DownloadException.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace CommunityTranslation\RemotePackage;
 
-use Exception;
+use Concrete\Core\Error\UserMessageException;
 
 defined('C5_EXECUTE') or die('Access Denied.');
 
-class DownloadException extends Exception
+class DownloadException extends UserMessageException
 {
     public function __construct(string $message, int $httpCode)
     {
